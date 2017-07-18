8348b68d3e5b16a56d27ac2bd64858e4f2792ffd
Aditya Chetan <achetan40@gmail.com>
2017-07-18 23:11:27 +0530

    Added a form "add.html" to take in inputs

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="This title has been passed from a view to template", text=["first","second","third"])
 
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
 if __name__=="__main__":
     app.run(debug=True)
