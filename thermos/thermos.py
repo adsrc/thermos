182bd8bf90bc7889b9912c2e902b11d4682ff64f
Aditya Chetan <achetan40@gmail.com>
2017-07-25 22:55:42 +0530

    added a comment

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    """render_template takes an optional argument, template that can be used to pass data to the front end"""
     return render_template('index.html', title="This title has been passed from a view to template", text=["first","second","third"])
 
 
 @app.route('/add')
 def add():
     return render_template('add.html')
-
 
 if __name__=="__main__":
     app.run(debug=True)
